4e45978d89fd37d79024d0782da2f37a9cd34ee4
Mike Eng <mrengy@gmail.com>
2015-12-10 18:47:09 -0500

    attempting responsive canvas sizing

--- js/main.js
+++ js/main.js
@@ -1,35 +1,18 @@
 $( document ).ready(function() {
-    preloadCursors(); 
-    $('audio#eel').trigger('load');
-    var spotCounter = 1;
-	var eelWidth=127;
-	var eelHeight=127;
+    //set canvas context
+        //var canvas = (typeof(G_vmlCanvasManager) != 'undefined') ? G_vmlCanvasManager.initElement($("canvas#card")[0]) : $("canvas#card")[0];
+        var canvas = $('canvas#card')[0];
+        ctx = canvas.getContext('2d');
+        ctx.font = "20.0px Arial, Helvetica, sans-serif";
+        var container = $(canvas).parent();
+        
+    //reset height and width
+    $(window).resize(respondCanvas);
 
-    $('#face-container').mousedown(function(event){
-    	$(this).addClass('punching');
-    	$(this).append($('#original-spot').clone().removeAttr('id').attr('id','spot-'+spotCounter).css('left', event.pageX - eelWidth).css('top', event.pageY - eelHeight));
-    	$('audio#eel').trigger('play');
-        spotCounter++;
-    });
-    $('#face-container').mouseup(function(){
-    	$(this).removeClass('punching');
-    });
-
-    function preloadCursors(){
-         // counter
-         var i = 0;
-
-         // create object
-         imageObj = new Image();
-
-         // set image list
-         images = new Array();
-         images[0]='img/cursor_eel_rotated.png';
-
-         // start preloading
-         for(i=0; i<=(images.length-1); i++) 
-         {
-              imageObj.src=images[i];
-         }
+    function respondCanvas(){
+        canvas.attr('width', $(container).width() );
+        canvas.attr('height', $(container).height() );
     }
+    //initial call
+    respondCanvas();
 });
