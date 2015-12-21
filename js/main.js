14cf5ff6e19a96393215cbaaaec16be0167b895b
Mike Eng <mrengy@gmail.com>
2015-12-20 21:39:35 -0500

    adding audio context

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 $( document ).ready(function() {
+    // create the audio context (chrome only for now)
+    if (! window.AudioContext) {
+        if (! window.webkitAudioContext) {
+            alert('no audiocontext found');
+        }
+        window.AudioContext = window.webkitAudioContext;
+    }
+
+    //audio context variables
+    var context = new AudioContext();
+    var audioBuffer;
+    var sourceNode;
+    var splitter;
+    var analyser, analyser2;
+    var javascriptNode;
+
+    var musicStarted = false;
+    var averageVolume = averageVolume2 = 0;
+    
+    var ctx;
+    var WIDTH;
+    var HEIGHT;
+    var intervalId = 0;
+    var frame = 0;
+    
+    var increment = 1;
+    
+    var volumeCenter = 70;
+    
+    var framesBetweenShift = 30;
+
     //set canvas context
         //var canvas = (typeof(G_vmlCanvasManager) != 'undefined') ? G_vmlCanvasManager.initElement($("canvas#card")[0]) : $("canvas#card")[0];
         var canvas = $('canvas#card')[0];
